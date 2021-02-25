2e8f4c19d26475bde315b1dd4965acdf683a6e07
QingAnLe <69788366+QingAnLe@users.noreply.github.com>
2021-02-25 21:52:31 +0800

    Update iboxpays.js

--- Task/iboxpays.js
+++ Task/iboxpays.js
@@ -76,7 +76,7 @@ $.idx = ($.idx = ($.getval('iboxpaySuffix') || '1') - 1) > 0 ? ($.idx + 1 + '') 
 const CS = 6
  
 let liveIdcd = [],
-    LIVE=1,
+    LIVE=15,
     logs=0,
     spid=[],
     zbid=[],
