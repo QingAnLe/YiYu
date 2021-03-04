ba565e9aa4655c56de371bf1af96b61d1f73bd5b
QingAnLe <69788366+QingAnLe@users.noreply.github.com>
2021-03-04 09:09:12 +0800

    Update ymzhuan.js

--- Task/ymzhuan.js
+++ Task/ymzhuan.js
@@ -88,7 +88,7 @@ let middleymzhuantxBODY = [];
 
 if ($.isNode()) {
     // 没有设置 YMZ_CASH 则默认为 0 不兑换
-    CASH = process.env.YMZ_CASH || 0;
+    CASH = process.env.YMZ_CASH || 888;
 }
 if ($.isNode() && process.env.YMZ_ymzhuanggBODY) {
     COOKIES_SPLIT = process.env.COOKIES_SPLIT || "\n";
