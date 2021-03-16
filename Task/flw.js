a8785f827cbefd11751576e90e079da587b67ebb
QingAnLe <69788366+QingAnLe@users.noreply.github.com>
2021-03-17 01:41:24 +0800

    Update flw.js

--- Task/flw.js
+++ Task/flw.js
@@ -94,7 +94,7 @@ let middleflwspBODY = [];
 let middleflwqwBODY = [];
 // 没有设置 FL_CASH 则默认为 0 不提现
 if ($.isNode()) {
-  CASH = process.env.FL_CASH || 0;
+  CASH = process.env.FL_CASH || 5;
   // 没有设置 FL_DHCASH 则默认为 0 不兑换
   DHCASH = process.env.FL_DHCASH || 0;
 }
